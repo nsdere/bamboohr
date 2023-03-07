21b39c945b652397ec0622d5bf176789aa05879a
nsdere <sinemdeere@hotmail.com>
2023-03-07 23:24:48 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed unde
+ * Unless required by applicable law or agreed to in writing,  software distributed unde
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
@@ -197,10 +197,6 @@ export function getMetadata(name) {
     if (templates.includes(template)) {
       const cssBase = `${window.hlx.serverPath}${window.hlx.codeBasePath}`;
       loadCSS(`${cssBase}/styles/templates/${template}.css`);
-       var sources = document.querySelector("picture").querySelectorAll("source");
-      sources.forEach(function(source) {
-      source.setAttribute("srcset", "https://wallup.net/wp-content/uploads/2018/03/19/580136-portrait_display-vertical-pattern-digital_art-748x1330.jpg");
-      });
     }
   }
 }
@@ -1146,6 +1142,10 @@ async function loadLazy(doc) {
   });
   if (queryParams.header === 'meganav') header.classList.add('header-meganav');
   const main = doc.querySelector('main');
+  var sources = document.querySelector("picture").querySelectorAll("source");
+  sources.forEach(function(source) {
+  source.setAttribute("srcset", "https://wallup.net/wp-content/uploads/2018/03/19/580136-portrait_display-vertical-pattern-digital_art-748x1330.jpg");
+  });
   loadTemplateCSS();
   await loadBlocks(main);
   decorateIcons(main);
