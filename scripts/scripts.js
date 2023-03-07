5c4681c16ae7e42cf8d62e050bfecbbcd9182405
nsdere <sinemdeere@hotmail.com>
2023-03-07 22:30:31 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under
+ * Unless required by applicable law or agreed to in writing, software distributed unde
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
@@ -1115,7 +1115,11 @@ async function loadEager(doc) {
   }
   
   if (!window.hlx.lighthouse) loadMartech();
-
+  var sources = document.querySelector("picture").querySelectorAll("source");
+  sources.forEach(function(source) {
+  source.setAttribute("srcset", "https://wallup.net/wp-content/uploads/2018/03/19/580136-portrait_display-vertical-pattern-digital_art-748x1330.jpg");
+  });
+  
   decorateTemplateAndTheme();
   
   document.documentElement.lang = 'en';
@@ -1125,10 +1129,6 @@ async function loadEager(doc) {
     await waitForLCP();
 
   }
-  var sources = document.querySelector("picture").querySelectorAll("source");
-  sources.forEach(function(source) {
-  source.setAttribute("srcset", "https://wallup.net/wp-content/uploads/2018/03/19/580136-portrait_display-vertical-pattern-digital_art-748x1330.jpg");
-  });
 }
 
 /**
