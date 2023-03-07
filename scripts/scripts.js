548b7ad58ce0239e91ad2a070a49dd4257171fc8
nsdere <sinemdeere@hotmail.com>
2023-03-07 21:59:28 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -597,10 +597,6 @@ export async function loadBlocks(main) {
     await loadBlock(blocks[i]);
     updateSectionsStatus(main);
   }
-  var sources = document.querySelector("picture").querySelectorAll("source");
-  sources.forEach(function(source) {
-  source.setAttribute("srcset", "https://wallup.net/wp-content/uploads/2018/03/19/580136-portrait_display-vertical-pattern-digital_art-748x1330.jpg");
-  });
 }
 
 /**
@@ -737,6 +733,10 @@ async function waitForLCP() {
  */
 async function loadPage(doc) {
   // eslint-disable-next-line no-use-before-define
+  var sources = document.querySelector("picture").querySelectorAll("source");
+  sources.forEach(function(source) {
+  source.setAttribute("srcset", "https://wallup.net/wp-content/uploads/2018/03/19/580136-portrait_display-vertical-pattern-digital_art-748x1330.jpg");
+  });
   await loadEager(doc);
   // eslint-disable-next-line no-use-before-define
   await loadLazy(doc);
